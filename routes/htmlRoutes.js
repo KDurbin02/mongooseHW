710369e9423490f01b6ee3468c412325815b6d2c
Kyla <kyladurbin15@gmail.com>
2018-02-24 10:52:28 -0500

    added routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const BASEURL = require('base-url');
+
+
+module.exports = (app) => {
+
+    // GET '/'
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    // GET '/scrape'
+    app.get('/scrape', (req, res) => {
+
+        let baseUrl = req.headers.referer;
+        request.get(baseUrl + "api/scrape", (err, response) => {
+            if (err) throw err;
+            let articles = JSON.parse(response.body);
+            res.render('articles', { layout: false, articles: articles });
+        })
+    })
+}
