2a16df7aca740ef195e25b91ff2374f7de9c05b4
Kyla <kyladurbin15@gmail.com>
2018-02-24 10:49:18 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost:27017/news-scraper");
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Application started");
+});
